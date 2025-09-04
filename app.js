6a8ce35116a3bdabf3652f108db9e5e74bd50fcc
TheDevilzz <bass02.bow@gmail.com>
2025-09-04 09:22:39 +0700

    update 1

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const debug = require('debug')('app');
 const app = express();
 const morgan = require('morgan');
 const port = 3000;
-
+const path = require('path');
 app.use(morgan('combined'));
-
+app.use(express.static(path.join(__dirname, '/public/')));
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
